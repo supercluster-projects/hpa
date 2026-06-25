a731d6e0658c1ef93fc39128e1ce761e18d09ff6
Vladimir Cores <vladimir.cores@gmail.com>
2026-06-26 00:08:47 +0300

    CEPH_IMAGE -> CEPH_VERSION: use version var like all other components
    CEPH_IMAGE is now derived inside install-rook-ceph.sh as:
      CEPH_IMAGE="quay.io/ceph/ceph:"
    
    Consistent with other components that use _VERSION vars.
    --ceph-image CLI flag kept for full image override.

--- provisioning/scripts/install-rook-ceph.sh
+++ provisioning/scripts/install-rook-ceph.sh
@@ -22,7 +22,7 @@
 
 # ---- Required environment variables (fail fast if missing from .env) ---
 require_env ROOK_VERSION
-require_env CEPH_IMAGE
+require_env CEPH_VERSION
 require_env DEV_CIDR_BLOCK
 require_env DEV_CLUSTER_NAME
 require_env DEV_NODE_PREFIX
@@ -36,7 +36,7 @@ POOL_CIDR="${DEV_CIDR_BLOCK}"
 WAIT_TIMEOUT=600
 HELM_RELEASE_NAME="rook-ceph"
 HELM_NAMESPACE="rook-ceph"
-CEPH_IMAGE="${CEPH_IMAGE}"
+CEPH_IMAGE="quay.io/ceph/ceph:${CEPH_VERSION}"
 
 # ---- CLI Overrides --------------------------------------------------------
 while [[ $# -gt 0 ]]; do
